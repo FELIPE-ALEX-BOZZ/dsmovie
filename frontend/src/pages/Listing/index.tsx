de339f74717df38a914e64bb9a9a623055498a6e
FELIPE-ALEX-BOZZ <familiabozz@gmail.com>
2022-05-14 22:55:34 -0300

    Movie Stars

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,14 @@
 import Pagination from "components/Pagination";
+import MovieStars from "components/MovieStars";
 
 function Listing() {
 
-    return(
-        <Pagination />
+    return (
+        <>
+            <Pagination />
+            <MovieStars />
+        </>
+
     );
 }
 
